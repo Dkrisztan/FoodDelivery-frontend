40730cb6dff1b784f8549eb865f814106c4d61d1
Dkrisztan <krisztidancs@gmail.com>
2024-02-16 16:38:25 +0100

    might switch to react and vite

--- frontend/components/Register.tsx
+++ frontend/components/Register.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { useState } from 'react';
-import axios from 'axios';
-import { redirect } from 'next/navigation';
 
-export function Login() {
+export function Register({onSubmit}) {
   const [formData, setFormData] = useState({
+    name: '',
     email: '',
     password: ''
   });
@@ -15,33 +14,36 @@ export function Login() {
       [e.target.name]: e.target.value,
     });
   };
-
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-
-    console.log(formData);
-
-    try {
-      const response = await axios.post('http://localhost:3000/user', formData);
-      console.log(response);
-      redirect("/login")
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  };
-
+  
   return (
     <>
       <div className="flex items-center">
         <div className="flex min-h-full flex-1 flex-col justify-center">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign In
+              Register an account
             </h2>
           </div>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+            <form className="space-y-6" onSubmit={onSubmit} method="POST">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    onChange={handleChange}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -89,7 +91,7 @@ export function Login() {
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign In
+                  Register
                 </button>
               </div>
             </form>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,21 @@
-import { Login } from "../../components/Login";
+import { Register } from '../../components/Register';
+import axios from 'axios';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/user', formData);
+      console.log(response);
+      redirect("/login")
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
   return (
     <>
-      <Login />
+      <Register onSubmit={handleSubmit}/>
     </>
   );
 }
