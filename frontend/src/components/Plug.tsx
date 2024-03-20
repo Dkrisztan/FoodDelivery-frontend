a91e5b0687e3f690c3b77269772c5095c74c5c67
Dkrisztan <krisztidancs@gmail.com>
2024-03-20 21:42:33 +0100

    dialog for updating plug

--- frontend/src/components/Plug.tsx
+++ frontend/src/components/Plug.tsx
@@ -1,4 +1,25 @@
 import Icons from '../utils/Icons.tsx';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog.tsx';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button.tsx';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select.tsx';
+import { useState } from 'react';
+import { HexColorPicker } from 'react-colorful';
 
 interface PlugProps {
   name: string;
@@ -7,18 +28,74 @@ interface PlugProps {
   icon: string;
 }
 export function Plug({ name, color, index, icon }: PlugProps) {
+  const [colorPicker, setColorPicker] = useState('#7e4242');
+
   let alternatingBackground = '#F1DFD0';
   if (index % 2 !== 0) alternatingBackground = '#F5E8DD';
   return (
     <>
       <div className='container mx-auto mt-4 mb-4 flex flex-row h-auto'>
-        <div
-          style={{ backgroundColor: color }}
-          className='h-1/4 w-1/6 py-2 px-5 flex flex-row items-center justify-between rounded-l-xl'
-        >
-          <p className='text-2xl mx-auto'>{name}</p>
-          <img src={Icons[icon]} width={32} height={32} alt='icon' />
-        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <div
+              style={{ backgroundColor: color }}
+              className='h-1/4 w-1/6 py-2 px-5 flex flex-row items-center justify-between rounded-l-xl'
+            >
+              <p className='text-2xl mx-auto'>{name}</p>
+              <img src={Icons[icon]} width={32} height={32} alt='icon' />
+            </div>
+          </DialogTrigger>
+          <DialogContent className='w-1/5 bg-neutral-800 text-white border-none'>
+            <DialogHeader>
+              <DialogTitle className='text-center'>Konnektor szerkesztése</DialogTitle>
+            </DialogHeader>
+            <div className='flex flex-row py-2 justify-evenly'>
+              <div className='flex flex-row items-center justify-between gap-4'>
+                <Label htmlFor='name' className='text-right'>
+                  Név
+                </Label>
+                <Input type='text' id='name' className='bg-neutral-800 text-white' />
+              </div>
+              <div>
+                <Select>
+                  <SelectTrigger className='w-[40px] p-0 outline-none border-none focus:outline-none focus:shadow-none bg-neutral-800'>
+                    <SelectValue
+                      placeholder={
+                        <img alt='icon' height='64' src={Icons['microwave']} width='64' />
+                      }
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value='icon'>
+                        <img alt='icon' height='64' width='64' src={Icons['microwave']} />
+                      </SelectItem>
+                      <SelectItem value='banana'>
+                        <img alt='icon' height='64' width='64' src={Icons['oven']} />
+                      </SelectItem>
+                      <SelectItem value='blueberry'>
+                        <img alt='icon' height='64' width='64' src={Icons['refrigerator']} />
+                      </SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+            <HexColorPicker
+              color={colorPicker}
+              onChange={setColorPicker}
+              className='react-colorful'
+            />
+            <DialogFooter>
+              <Button
+                className='mx-auto bg-gray-200 text-black font-bold hover:bg-gray-300'
+                type='submit'
+              >
+                Változtatások mentése
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
 
         <div
           style={{ backgroundColor: alternatingBackground }}
--- frontend/src/components/TimeLine.tsx
+++ frontend/src/components/TimeLine.tsx
@@ -1,10 +1,13 @@
+import watch from '../assets/wristwatch.png';
+
 export function TimeLine() {
   const hours = [6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 2, 4];
   return (
     <>
       <div className='container mx-auto flex flex-row items-center justify-between rounded-xl'>
-        <div className='w-1/6 flex flex-row items-center justify-between rounded-l-xl bg-gray-300'>
-          <p className='text-2xl mx-auto'>Óra ⌚</p>
+        <div className='w-1/6 flex flex-row items-center justify-between rounded-l-xl bg-gray-300 px-5'>
+          <p className='text-2xl mx-auto'>Óra</p>
+          <img src={watch} width={32} height={32} />
         </div>
         <div className='grid grid-cols-12 flex-grow h-1/4 px-10 gap-6 rounded-r-xl text-2xl bg-gray-300'>
           {hours.map((hour) => (
@@ -12,17 +15,6 @@ export function TimeLine() {
           ))}
         </div>
       </div>
-      {/*<div className='container mx-auto flex flex-row'>*/}
-      {/*  <div className='w-1/6 flex flex-row items-center justify-between rounded-l-xl bg-gray-300'>*/}
-      {/*    <p className='text-2xl mx-auto'>Óra ⌚</p>*/}
-      {/*  </div>*/}
-
-      {/*  <div className='grid grid-cols-12 flex-grow h-1/4 px-10 gap-6 rounded-r-xl text-2xl bg-gray-300'>*/}
-      {/*    {hours.map((hour) => (*/}
-      {/*      <div key={hour}>{hour + 'h'}</div>*/}
-      {/*    ))}*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </>
   );
 }
