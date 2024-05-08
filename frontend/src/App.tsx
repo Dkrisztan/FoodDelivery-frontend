97d16e539ac49a550eb755b21879c337a3114d3c
Dkrisztan <krisztidancs@gmail.com>
2024-05-08 21:59:43 +0200

    vercel deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Login } from './components/Login.tsx';
 import { Home } from './components/Home.tsx';
 import { Registration } from './components/Registration.tsx';
 import { Profile } from './components/Profile.tsx';
-import { DataTest } from './components/DataTest.tsx';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path='login' element={<Login />} />
         <Route path='register' element={<Registration />} />
         <Route path='profile' element={<Profile />} />
-        <Route path='test' element={<DataTest />} />
       </Routes>
     </BrowserRouter>
   );
